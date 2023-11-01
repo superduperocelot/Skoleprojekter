450ca854973b9ee7d422b86ad158576e9b5a0f10
superduperocelot <57543556+superduperocelot@users.noreply.github.com>
2023-11-01 12:47:57 +0100

    Delete PYTHON directory

--- PYTHON/Cookie counter.py
+++
@@ -1,6 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-url = "https://orteil.dashnet.org/cookieclicker/"
-req = requests.get(url)
-soup = BeautifulSoup(req.text, "html.parser")
-print(soup.title)
